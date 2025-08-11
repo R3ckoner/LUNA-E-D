2ae32cc445005f47d7bc9f5e94ae83c2e1ed5035
Ruairi Delahunt <ruairidelahunt@gmail.com>
2025-08-10 21:47:45 -0700

    Initial Commit

---
+++ EliteCompanion/EliteCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliteCompanionApp.swift
+//  EliteCompanion
+//
+//  Created by Ruairi Delahunt on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EliteCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
